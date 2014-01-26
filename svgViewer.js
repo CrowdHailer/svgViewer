09c3dfb43e1c55b31c414ff0afc8e0e9eed405ee
CrowdHailer <peterhsaxton@gmail.com>
2014-01-26 18:00:51 +0000

    touch handler check for svg children

--- svgViewer.js
+++ svgViewer.js
@@ -4,7 +4,13 @@ var svgViewer = {
 		
 		if (!this.isSVGElement(containerElement)) return;
 		
-		this.svgElement = containerElement;
+		this.liveElement = containerElement;
+		
+		this.hammertime = Hammer(document).on('touch', this.touchHandler);
+	},
+	touchHandler: function(evt) {
+		if (evt.target.ownerSVGElement != svgViewer.liveElement) return;
+		console.log('correct target');
 	},
 	isSVGElement: function (element) {
 		return element.tagName.toLowerCase() == 'svg';
